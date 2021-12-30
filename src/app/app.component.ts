7c95796fa5290decd81af5d38e01f18e589a3c41
Elizaveta Ivanova <lizuha1860@gmail.com>
2021-12-30 19:36:31 +0300

    main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'my-app',
-    template: `<input [(ngModel)]="name">>`
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-    name = '';
 }
