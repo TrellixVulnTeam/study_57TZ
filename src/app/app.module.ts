5e58dc95e4d2a235fcfa11e0e9301c7059a469d6
Elizaveta Ivanova <lizuha1860@gmail.com>
2022-01-25 22:49:27 +0300

    popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+
 import { AppComponent } from "./app.component";
 
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
-    imports: [BrowserModule, FormsModule],
+    imports: [
+        BrowserModule, 
+        FormsModule, 
+        RouterModule,
+        AppRoutingModule,
+    ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
